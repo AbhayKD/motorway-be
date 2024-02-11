706378bb7ce56b32eef482d36e566b479eee4a9f
Abhay Dekate <abhaydekate@abhays-air.home>
2024-02-11 22:56:15 +0000

    Adding eslint, tsconfig, prettier config and gitignore

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 120,
+  tslintintegration: true,
+  trailingComma: 'all',
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+}
